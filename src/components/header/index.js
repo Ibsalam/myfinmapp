fe5c32173de7c5229b2f2e1ee6935c8f2eb4063a
ibsalam <asio4edu@gmail.com>
2024-10-14 20:14:47 +0100

    updated log out in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import "./styles.css";
 function Header() {
+    function logOutFunction() {
+        alert("Logged Out")
+    }
+
     return <div className="navbar">
         <p className="logo">MyfinmApp</p>
+        <p className="logo link" onClick={logOutFunction}>Logout</p>
     </div>;
 }
 
