2825100e6c42e7057d9e99e45bc61c160468f97c
ibsalam <asio4edu@gmail.com>
2024-10-15 08:25:08 +0100

    develop the sign up page

---
+++ src/components/signupSignin/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function SignupSigninComponent() {
+    return <div className="signup-wrapper">SignupSigninComponent</div>
+}
+
+export default SignupSigninComponent;
